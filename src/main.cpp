888bbd5eed53b9b1d796c9e3c2699dc62566b418
Jay Patel <itsjay0730@gmail.com>
2026-05-16 17:02:29 -0500

    opensky api setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "hi" << endl;
     
     return 0;
 }
---
+++ src/telemetry/fetchers/OpenSky.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+class OpenSkyFetcher{
+public:
+    std::string fetchOpenSky() {
+
+    }
+};
+
