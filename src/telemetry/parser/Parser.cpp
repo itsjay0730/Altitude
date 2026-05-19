6f8d47969a946689fa8879aed1a5db000f5571eb
Jay Patel <itsjay0730@gmail.com>
2026-05-19 13:45:41 -0500

    open sky parser updates

--- src/telemetry/parser/Parser.cpp
+++ src/telemetry/parser/Parser.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <nlohmann/json.hpp>
 #include "Parser.hpp"
 
 std::vector<AircraftState> Parser::parseOpenSky(const std::string& json){
+    nlohmann::json data = nlohmann::json::parse(json);
+    nlohmann::json states = data["states"];
+
+    std::vector<AircraftState> vec;
+    
 
 }
