5383e28814ddddc66371571e781b398013e396df
Jay Patel <itsjay0730@gmail.com>
2026-05-19 14:25:21 -0500

    print function to test struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <vector>
 #include "telemetry/fetcher/Fetcher.hpp"
+#include "telemetry/parser/Parser.hpp"
+
+void printPlane(AircraftState plane) {
+    std::cout << "----------------------------------" << std::endl;
+
+    std::cout << "ICAO24: " << plane.icao24 << std::endl;
+    std::cout << "Callsign: " << plane.callsign << std::endl;
+    std::cout << "Country: " << plane.originCountry << std::endl;
+    std::cout << "Time: " << plane.time << std::endl;
+    std::cout << "Last Contact: " << plane.lastContact << std::endl;
+    std::cout << "Longitude: " << plane.longitude << std::endl;
+    std::cout << "Latitude: " << plane.latitude << std::endl;
+    std::cout << "Altitude: " << plane.altitude << std::endl;
+    std::cout << "Velocity: " << plane.velocity << std::endl;
+    std::cout << "Heading: " << plane.direction << std::endl;
+    std::cout << "Vertical Rate: " << plane.altitudeRate << std::endl;
+    std::cout << "In Air: " << plane.inAir << std::endl;
+    std::cout << "Squawk: " << plane.squawk << std::endl;
+    std::cout << "SPI: " << plane.spi << std::endl;
+    std::cout << "Source: " << plane.source << std::endl;
+
+    std::cout << "----------------------------------" << std::endl;
+}
 
 int main() {
     Fetcher fetcher;
+    Parser parser;
 
     std::string results = fetcher.fetchOpenSky();
-    std::cout << results << std::endl;
+    
+    std::vector<AircraftState> planes = parser.parseOpenSky(results);
+    for(int i = 0; i < 3; i++) {
+        printPlane(planes[i]);
+    }
     
     return 0;
 }
