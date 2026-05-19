cae1266045d457c88ca329664f68540cc6491ac7
Jay Patel <itsjay0730@gmail.com>
2026-05-19 13:57:20 -0500

    convert json to aircraftstate

--- src/telemetry/parser/Parser.cpp
+++ src/telemetry/parser/Parser.cpp
@@ -9,5 +9,20 @@ std::vector<AircraftState> Parser::parseOpenSky(const std::string& json){
 
     std::vector<AircraftState> vec;
     
+    for(const auto& state : states) {
+        AircraftState aircraft;
+        
+        aircraft.icao24 = state[0];
+        aircraft.callsign = state[1];
+        aircraft.originCountry = state[2];
+        aircraft.longitude = state[5];
+        aircraft.latitude = state[6];
+        aircraft.altitude = state[7];
+        aircraft.velocity = state[9];
+        aircraft.direction = state[10];
+        aircraft.altitudeRate = state[11];
+
+        vec.push_back(aircraft);
+    }
 
 }
