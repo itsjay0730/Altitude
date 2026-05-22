82cf7872c0eca52589da75a32a3e3ea30e21167e
Jay Patel <itsjay0730@gmail.com>
2026-05-22 15:35:29 -0500

    updated main.py with router from enrichment

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.routes.enrichment import router as enrichmentRouter
 
 app = FastAPI()
+
+app.include_router(enrichmentRouter)
--- backend/app/routes/enrichment.py
+++ backend/app/routes/enrichment.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 @router.get("/enrichment/{icao24}")
 def enrichment(icao24: str):
-    url = f"https://airlabs.co/api/v9/flights?api_key={API_KEY}&hex={icao24}"
+    url = f"https://airlabs.co/api/v9/flights?api_key={AIRLABS_API_KEY}&hex={icao24}"
 
     response = requests.get(url)
     return response.json()
