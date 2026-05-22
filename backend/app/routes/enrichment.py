5019413829e5555e511e024e5c4f4c896e2c7c6e
Jay Patel <itsjay0730@gmail.com>
2026-05-22 15:47:49 -0500

    updated endpoint url to test enrichment

--- backend/app/routes/enrichment.py
+++ backend/app/routes/enrichment.py
@@ -8,9 +8,9 @@ AIRLABS_API_KEY = os.getenv("AIRLABS_API_KEY")
 
 router = APIRouter()
 
-@router.get("/enrichment/{icao24}")
-def enrichment(icao24: str):
-    url = f"https://airlabs.co/api/v9/flights?api_key={AIRLABS_API_KEY}&hex={icao24}"
+@router.get("/enrichment/{icao}")
+def enrichment(icao: str):
+    url = f"https://airlabs.co/api/v9/flights?api_key={AIRLABS_API_KEY}&hex={icao}"
 
     response = requests.get(url)
     return response.json()
