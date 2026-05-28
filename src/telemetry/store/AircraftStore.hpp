80f202cc4133a743ae1dc96207d8e41e687b30f6
Jay Patel <itsjay0730@gmail.com>
2026-05-28 18:50:12 -0500

    aircraftstore hpp created + defined

---
+++ src/telemetry/store/AircraftStore.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <unordered_map>
+#include <vector>
+#include "../models/AircraftState.hpp"
+
+class AircraftStore {
+private:
+    std::unordered_map<std::string, AircraftState> map;
+
+public:
+
+    void update(const std::vector<AircraftState>& aircrafts);
+
+    std::vector<AircraftState> getAll();
+};
