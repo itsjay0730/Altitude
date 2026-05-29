7d917afb58d719a472da8ac3de77fbcb09cc8b7e
Jay Patel <itsjay0730@gmail.com>
2026-05-29 16:20:38 -0500

    live telemetry engine integrated

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <chrono>
+
 #include "telemetry/fetcher/Fetcher.hpp"
 #include "telemetry/parser/Parser.hpp"
+#include "telemetry/store/AircraftStore.hpp"
 
 void printPlane(AircraftState plane) {
     std::cout << "----------------------------------" << std::endl;
@@ -28,6 +32,28 @@ void printPlane(AircraftState plane) {
 int main() {
     Fetcher fetcher;
     Parser parser;
+    AircraftStore store;
+
+    while(true) {
+        try {
+            std::string jsonResp = fetcher.fetchOpenSky();
+            std::vector<AircraftState> aircrafts = parser.parseOpenSky(jsonResp);
+
+            store.update(aircrafts);
+            store.removeStale();
+
+            std::vector<AircraftState> allAircrafts = store.getAll();
+
+            //Just a test
+            std::cout << "Live Aircrafts: " << allAircrafts.size() << std::endl;
+
+        }catch(const std::exception& e) {
+            std::cout << "Error: " << e.what() << std::endl;
+        }
+
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
 
     std::string results = fetcher.fetchOpenSky();
     
