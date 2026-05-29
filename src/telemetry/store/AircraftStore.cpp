32c26b915c3b89300d5c943cd52f60f40df6ccde
Jay Patel <itsjay0730@gmail.com>
2026-05-29 15:47:37 -0500

    finished aircraftstore.cpp function definitions

--- src/telemetry/store/AircraftStore.cpp
+++ src/telemetry/store/AircraftStore.cpp
@@ -1,7 +1,32 @@
+#include <ctime>
 #include "AircraftStore.hpp"
 
 void AircraftStore::update(const std::vector<AircraftState>& aircrafts) {
     for(const auto& aircraft : aircrafts) {
         map[aircraft.icao24] = aircraft;
     }
+};
+
+void AircraftStore::removeStale() {
+    long currTime = std::time(nullptr);
+
+    for(auto it = map.begin(); it != map.end();) {
+        long age = currTime - it->second.lastContact;
+
+        if(age > 300) {
+            it = map.erase(it);
+        }else {
+            ++it;
+        }
+    }
+};
+
+std::vector<AircraftState> AircraftStore::getAll() {
+    std::vector<AircraftState> vec;
+
+    for(const auto& pair : map) {
+        vec.push_back(pair.second);
+    }
+
+    return vec;
 };
